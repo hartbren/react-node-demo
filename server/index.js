5381e3c7da603e865a95c852db1cb4973435644e
Brendan Hart <brendan@restockrevlution.com>
2021-09-12 21:50:11 +0930

    Server API endpoints first pass

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ process.on('uncaughtException', function(err) {
 // import packages
 const path = require('path');
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,10 +17,33 @@ const app = express();
 // Have Node serve the static files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server! x" });
+
+// data store in memory
+let messages = [];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/api/send-message', (req, res) => {
+
+    const msg = req.body;
+
+    // Output the book to the console for debugging
+    console.log(msg);
+    messages.push(msg);
+
+    res.send('OK');
 });
+
+// Handle GET requests to /api route
+app.get("/api/list-messages", (req, res) => {
+
+    res.json(messages);
+});
+
 
 // Handle all other GET requests: return our React app
 app.get("*", (req, res) => {
